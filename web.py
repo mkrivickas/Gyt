3025af4d1040f7a9e4e3fc685abd3afbd992f580
Mindaugas Krivickas <mkrivickas@gmail.com>
2023-07-12 19:09:54 +0300

    Update web.py
    Entry deployed on top of todos list.

--- web.py
+++ web.py
@@ -13,6 +13,9 @@ st.title("My Todo App")
 st.subheader("This is my todo app.")
 st.write("This app is to increase your productivity.")
 
+st.text_input(label="", placeholder="Add new todo...",
+              on_change=add_todo, key="new_todo")
+
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
     if checkbox:
@@ -21,5 +24,3 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.experimental_rerun()
 
-st.text_input(label="", placeholder="Add new todo...",
-              on_change=add_todo, key="new_todo")
